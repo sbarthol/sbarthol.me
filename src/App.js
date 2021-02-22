fdaf7dd9ee6e2ad73c561960f70d1ee749b77da3
Sacha Bartholme <sacha.bartholme@hotmail.com>
2021-02-22 21:15:33 +0100

    Added svg icons

--- src/App.js
+++ src/App.js
@@ -32,14 +32,15 @@ function App() {
       <Card
         skills={["C++", "VSCode"]}
         title="Finding customers for your new business"
-        text="Getting a new business off the ground is a lot of hard work. Here
-        are five ideas you can use to find your first customers."
+        text="rst customers."
         image="https://images.unsplash.com/photo-1515711660811-48832a4c6f69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
       />
       <Card
-        skills={["C++", "VSCode"]}
+        skills={["C++", "Java", "Concurrency", "Assembly"]}
         title="Finding customers for your new business"
         text="Getting a new business off the ground is a lot of hard work. Here
+        are five ideas you can use to find your first customers. Getting a new business off the ground is a lot of hard work. Here
+        are five ideas you can use to find your first customers. Getting a new business off the ground is a lot of hard work. Here
         are five ideas you can use to find your first customers."
         image="https://images.unsplash.com/photo-1515711660811-48832a4c6f69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80"
       />
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Launch from "./Launch.js";
+import Github from "./Github.js";
 
 function Card({ title, text, skills, image }) {
   return (
-    <div class="m-8 max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
+    <div class="m-8 max-w-md mx-auto bg-white rounded-xl shadow-lg overflow-hidden md:max-w-3xl">
       <div class="md:flex">
         <div class="md:flex-shrink-0">
           <img
@@ -11,11 +13,28 @@ function Card({ title, text, skills, image }) {
             alt="error"
           ></img>
         </div>
-        <div class="p-8">
-          <p class="block mt-1 text-lg leading-none font-medium text-black">
-            {title}
-          </p>
-          <p class="mt-2 text-gray-500">{text}</p>
+        <div class="block w-full">
+          <div class="p-6">
+            <p class="mt-1 text-xl leading-none font-bold text-black">
+              {title}
+            </p>
+            <p class="mt-2 text-gray-500">{text}</p>
+            <div class="mt-3 space-x-2 flex items-start justify-end">
+              <div class="flex space-x-2 flex-auto">
+                {skills.map((skill) => (
+                  <div class="w-min font-sans px-1 text-white rounded-md bg-indigo-500">
+                    {skill}
+                  </div>
+                ))}
+              </div>
+              <a href="https://github.com/sbarthol/scoogle">
+                <Launch />
+              </a>
+              <a href="https://github.com/sbarthol/scoogle">
+                <Github />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
