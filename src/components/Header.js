32f949a69f79e586c3059a2aa4a28449bff34777
Sacha Bartholme <sacha.bartholme@hotmail.com>
2021-02-23 18:43:43 +0100

    Better header on iphones

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div class="sticky top-0 shadow-lg w-full flex items-center h-auto bg-indigo-500">
+    <div class="md:sticky top-0 shadow-xl w-full flex items-center h-auto bg-indigo-500">
       <div class="font-mono tracking-tighter p-6 text-3xl font-medium text-white">
         Sacha Bartholme's open source portfolio in reverse chronological order
       </div>
